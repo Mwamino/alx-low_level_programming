eaf797c119a58d5c4304ef2a13630301940e4597
Mwamino <mwaminogideon@gmail.com>
2023-03-17 05:42:22 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Code entry
+ * main - Entry point
  *
- * Description: (prints with proper grammar, but the outcome is a piece of art,)
- * Return: 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+	return (0);
 }
