62c2841ab93b148440b9e6f514788bf134c00bd5
Mwamino <mwaminogideon@gmail.com>
2023-03-17 03:12:19 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Code is entered here
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
