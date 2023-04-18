0af57ad69e3da85768b23737ce36e8643dcc3ae9
Mwamino <mwaminogideon@gmail.com>
2023-04-18 02:43:48 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * file  0-onject_like_macro.h
+ * Description: Header file
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * file - 4-sum.h
+ * Description: Header file
+ * @x: value
+ * @y: value
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
