8f5d10dcec751ee3a653696acf54342e3486719e
Mwamino <mwaminogideon@gmail.com>
2023-05-03 03:15:03 -0700

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(nums, int));
 
 		if (index != (n - 1) && separator != NULL)
-		{
 			printf("%s", separator);
-	        }
 	}
+
 	printf("\n");
+
 	va_end(nums);
 }
