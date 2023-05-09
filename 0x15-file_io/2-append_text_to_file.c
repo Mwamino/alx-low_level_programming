d0391903d37b539f83816ddab26e16ff46f4e570
Mwamino <mwaminogideon@gmail.com>
2023-05-09 10:13:27 -0700

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,9 +9,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nwrite;
-	int len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 	{
@@ -19,20 +17,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-		{
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	nwrite = write(0, text_content, len);
-	if (fd == -1)
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 	{
 		return (-1);
 	}
-	if (nwrite == -1)
-	{
-		return (-1);
-	}
-	close(fd);
+	close(o);
 	return (1);
 }
